808dfd444e04f52b0418836a6ff80415efb34251
Pratyush Kumar Sethy <159880196+pratyushk22@users.noreply.github.com>
2025-06-03 15:52:30 +0530

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -1,20 +1,19 @@
 const mongoose = require('mongoose');
 
+const bookmarkedProblemSchema = new mongoose.Schema({
+  title: String,
+  url: String,
+  source: String, 
+  tags: [String],
+  dateBookmarked: { type: Date, default: Date.now }
+});
+
 const UserSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   email:    { type: String, unique: true, required: true },
   passwordHash: { type: String, required: true },
   bio: { type: String, default: "" },
-
-  savedProblems: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Problem' }],
-  
-  contributions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Problem' }],
-
-  bookmarks: [{
-    problemId: { type: mongoose.Schema.Types.ObjectId, ref: 'Problem' },
-    tags: [String],
-    notes: String
-  }]
+  bookmarks: [bookmarkedProblemSchema],
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
