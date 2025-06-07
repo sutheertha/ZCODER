784bd1b3e2e9f9dbcf59d88046c1b5499137508d
yagvalakya1975 <58080210+yagvalakya1975@users.noreply.github.com>
2025-06-07 21:57:45 +0530

    Update index.js

--- zcoder-frontend/src/index.js
+++ zcoder-frontend/src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './AuthContext';
-import { BrowserRouter as Router } from 'react-router-dom'; 
+import { BrowserRouter as Router } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/dracula.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
