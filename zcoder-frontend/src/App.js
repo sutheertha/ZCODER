ed574b80a77f5cb33027be41fdd69e5c737a69a7
yagvalakya1975 <58080210+yagvalakya1975@users.noreply.github.com>
2025-06-07 21:44:18 +0530

    Integrated Coderoom functionality

--- zcoder-frontend/src/App.js
+++ zcoder-frontend/src/App.js
@@ -1,47 +1,61 @@
-import { Switch, Route } from 'react-router-dom'; 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import './Coderoom/App.css';
 import Navbar from './Navbar';
 import SavedPage from './SavedPage';
 import LoginPage from './LoginPage';
 import ProfilePage from './ProfilePage';
 import PrivateRoute from './PrivateRoute';
+import Home from './Coderoom/component/Home';
+import EditorPage from './Coderoom/component/EditorPage';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <div className="App">
-      <div className="page">
-        <Switch>
-          <Route exact path="/">
-            <Navbar />
-            <h1>HOMEPAGE</h1>
-          </Route>
+    <Router>
+      <div className="App">
+        <Toaster position='top-center' />
+        <div className="page">
+          <Switch>
 
-          <PrivateRoute exact path="/saved">
-            <Navbar />
-            <SavedPage />
-          </PrivateRoute>
+            <Route exact path="/">
+              <Navbar />
+              <h1>HOMEPAGE</h1>
+            </Route>
+            
+            <Route exact path="/login">
+              <LoginPage authType="Login" />
+            </Route>
 
-          <Route exact path="/login">
-            <LoginPage authType="Login" />
-          </Route>
+            <PrivateRoute exact path="/saved">
+              <Navbar />
+              <SavedPage />
+            </PrivateRoute>
 
-          <PrivateRoute path="/profile/:userID">
-            <Navbar />
-            <ProfilePage />
-          </PrivateRoute>
+            <PrivateRoute path="/profile/:userID">
+              <Navbar />
+              <ProfilePage />
+            </PrivateRoute>
 
-          <PrivateRoute path="/editor">
-            <Navbar />
-            <h2>code editor</h2>
-          </PrivateRoute>
+            <PrivateRoute exact path="/editor">
+              <Navbar />
+              <h2>code editor</h2>
+            </PrivateRoute>
 
-          <PrivateRoute path="/chatroom">
-            <Navbar />
-            <h2>chatroom</h2>
-          </PrivateRoute>
-        </Switch>
+
+            <PrivateRoute exact path="/chatroom">
+              <Navbar />
+              <Home />
+            </PrivateRoute>
+
+            <PrivateRoute path="/chatroom/editor/:roomId">
+              <Navbar />
+              <EditorPage />
+            </PrivateRoute>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
